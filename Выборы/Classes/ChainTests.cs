5e40cc2a5c86118e20173e28324f400505d3eee1
Markellian <nikke.tv@rambler.ru>
2021-05-13 16:30:03 +0300

    Тестрирование работы с базой данных

---
+++ Выборы/Classes/ChainTests.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Выборы.Classes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Выборы.Classes.Tests
+{
+    [TestClass()]
+    public class ChainTests
+    {
+        [TestMethod()]
+        public void ChainConstructorTest()
+        {
+            Chain chain = new Chain(new Election("Выборы на пост главного дворника", DateTime.Parse("10.10.2019 20:00:00"), DateTime.Parse("10.12.2019 20:00:00")));
+            Assert.IsNotNull(chain);
+        }
+        [TestMethod()]
+        public void ChainAddTest()
+        {
+            Chain chain = new Chain(new Election("Выборы на пост главного дворника", DateTime.Parse("10.10.2019 20:00:00"), DateTime.Parse("10.12.2019 20:00:00")));
+            try
+            {
+                chain.Add(null, new Candidate());
+                Assert.Fail();
+            }
+            catch (ArgumentException ex)
+            {
+                Assert.AreEqual(Properties.Language.Invalid_user, ex.Message);
+            }
+            catch (Exception)
+            {
+                Assert.Fail();
+            }
+
+            try
+            {
+                chain.Add(new User(), null);
+                Assert.Fail();
+            }
+            catch (ArgumentException ex)
+            {
+                Assert.AreEqual(Properties.Language.Invalid_condidate, ex.Message);
+            }
+            catch (Exception)
+            {
+                Assert.Fail();
+            }
+
+            try
+            {
+                chain.Add(new User(), new Candidate());
+            }
+            catch (Exception)
+            {
+                Assert.Fail();
+            }
+        }
+
+    }
+}
---
+++ Выборы/Classes/ElectionTests.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Выборы.Classes;
+
+namespace Выборы.Tests
+{
+    [TestClass]
+    public class ElectionTests
+    {
+        [TestMethod]
+        public void GetElectionTest()
+        {
+            Election election = Election.GetElection("Этого_теста!!не^^^^^должно____существовать ни при каких обстоятельствах");
+            Assert.IsNull(election);
+
+            //election = DataBase.
+        }
+    }
+    
+}
